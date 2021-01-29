1af6add14aaed6f7905d2a6c2c841321c6826dd5
DESKTOP-7QOEEND\ryuma <weerawat.patt@bumail.net>
2021-01-29 23:54:13 +0700

    Assiment 1

---
+++ GI455_Project/Assets/Home work Week1/TextMach.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextMach : MonoBehaviour
+{
+    public Text addtext;
+    public Button search;
+    public InputField searchText;
+    public Text check;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        addtext.text = "Jim Pen Pencil Egg Gold";
+        search.onClick.AddListener(SearchText);
+
+
+    }
+   void SearchText()
+    {
+        string[] arr = { "Jim", "Pen", "Pencil","Egg","Gold" };
+        string str = searchText.text;
+        bool relese = arr.AsQueryable().Contains(str);
+        if (relese == true)
+        {
+            check.text = searchText.text + " found";
+        }
+        else
+        {
+            check.text = searchText.text + " not found";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
